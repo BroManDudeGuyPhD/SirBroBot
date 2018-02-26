1c0c4a6a8137d3b9f0f35e6280908581bbc72fe3
BroManDudeGuyPhD <>
2018-02-26 10:30:54 -0600

    Updated NLP response icons for social media responses

--- src/main/java/bromandudeguyphd/sirbrobot/nlpLibrary.java
+++ src/main/java/bromandudeguyphd/sirbrobot/nlpLibrary.java
@@ -66,10 +66,10 @@ public class nlpLibrary {
                         //SocialMedia intent                                                                           
                         if (response.getResult().getMetadata().getIntentId().equals("6b5a215a-f980-48b5-a97f-3e66690fe6e9")) {
                             returnedMessage = "**These are my social media links and website**   \n"
-                                    + "`Twitter`: <https://twitter.com/sirbrobotthe1st?lang=en>\n"
-                                    + "`Facebook`: <https://www.facebook.com/sirbrobot/>\n"
-                                    + "`YouTube`: <https://www.youtube.com/channel/UCZi_pzKLVb5zvTmDOCEMbtQ>\n"
-                                    + "`Website`: <http://bootswithdefer.tumblr.com/SirBroBot>";
+                                    + "<:SirBroBot_website:415563293340729345> <https://sirbrobot.com>"
+                                    + "<:SirBroBot_Twitter:415563272289517568> <https://twitter.com/sirbrobotthe1st?lang=en>\n"
+                                    + "<:SirBroBot_Facebook:417717351664975877> <https://www.facebook.com/sirbrobot/>\n"
+                                    + "<:SirBroBot_YouTube:415563282187943936> <https://www.youtube.com/channel/UCZi_pzKLVb5zvTmDOCEMbtQ>\n";
                         }
                         
                         
